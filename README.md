c9d714f642e87b6f2c2900c605d4187457874f12
DrEmixam <maxime@melinon.fr>
2017-01-01 14:33:19 +0100

    Nettoyages et optimisations

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Bot Twitter qui poste toutes les heures l'une des phrases du téléscripteur de Simcity 3000
 
+Le bot est en ligne sur https://twitter.com/tssc3k
+
 ## Utilisation
 
 Après avoir cloné le repo, copier le fichier config.example.json vers config.json et éditer le nouveau fichier pour y ajouter vos identifiants twitter (que vous obtiendrez en créant une application sur https://dev.twitter.com/)
--- tssc3k.js
+++ tssc3k.js
@@ -1,35 +1,35 @@
 var config = require('./config.json');
 var fs = require('fs');
-var Twitter     = require('twitter');
-var client      = new Twitter({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  access_token_key: config.access_token_key,
-  access_token_secret: config.access_token_secret
+var Twitter = require('twitter');
+var client = new Twitter({
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token_key: config.access_token_key,
+    access_token_secret: config.access_token_secret
 });
 
 function tweet() {
-  fs.readFile('phrases.txt', 'utf8', function(err, data){
-    if (err) {
-      console.log(err);
-    }
+    fs.readFile('phrases.txt', 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+        }
 
-    var lines = data.split('\n');
+        var lines = data.split('\n');
 
-    var line = lines[Math.floor(Math.random()*lines.length)];
+        var line = lines[Math.floor(Math.random() * lines.length)];
 
-    var status = {
-      status: line
-    };
+        var status = {
+            status: line
+        };
 
-    client.post('statuses/update', status, function(error, tweet, response) {
-      if (error) {
-        console.log(error);
-        console.log(status);
-      }
+        client.post('statuses/update', status, function (error, tweet, response) {
+            if (error) {
+                console.log(error);
+                console.log(status);
+            }
+        });
     });
-  });
 }
 
 tweet();
-setInterval(tweet,3600000);
+setInterval(tweet, config.interval);
